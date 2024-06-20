3b2273d5424ec7dd505c6938df40c1a4c444db0d
Ssnash86 <mendes.dylan86@yahoo.Fr>
2024-06-20 16:57:06 +0200

    update

---
+++ test.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+    <meta charset="utf-8" /> 
+    <title>GeeksforGeeks</title> 
+    <script src= 
+"https://cdn.tailwindcss.com"> 
+    </script> 
+</head> 
+<body class="bg-black"> 
+   
+<header class="relative flex items-center  
+            justify-center h-screen mb-12  
+            overflow-hidden"> 
+        
+        <video autoplay loop muted 
+            class="absolute z-10 w-auto  
+            min-w-full min-h-full max-w-none"> 
+            <source src= 
+"Asset/Ratatouille - La soupe.mp4"
+                type="video/mp4"/> 
+        </video> 
+    </header> 
+</body> 
+</html> 
